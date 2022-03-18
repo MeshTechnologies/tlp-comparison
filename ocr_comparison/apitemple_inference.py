ad18ad25843710c2a3c4a9f6c96248ebaa8c705e
Patryk Bartkowiak <patryk@pieces.app>
2022-03-18 10:08:02 +0100

    OCR comparison ready

--- ocr_comparison/apitemple_inference.py
+++ ocr_comparison/apitemple_inference.py
@@ -1,23 +1,29 @@
 import os
-import base64
 import requests
 
 
-def get_text(img):
+def get_text(img, key):
+    payload = {}
     return requests.get(
         url=f'https://api.apilayer.com/image_to_text/url?url={img}',
         headers={
-            'apikey': 'YOUR API KEY'
-        }
+            'apikey': key
+        },
+        data=payload
     ).json()
 
 
-def image_to_data_url(path):
-    return f"data:image/{path.split('.')[-1]};base64,{base64.b64encode(open(path, 'rb').read()).decode('utf-8')}"
-
-
 def main():
-    dataset_path = '30langs_200lines_fragmented'
+    # load user key for APITemple
+    key = os.getenv("TEMPLE_API_KEY")
+
+    for image in os.listdir(os.path.join('01-manually_labelled_code_screenshots', 'ocr', 'evaluation', 'images')):
+        if image.endswith('.png'):
+            url = f'https://www.linkpicture.com/q/{image}'
+            response = get_text(url, key)
+            if 'all_text' in response.keys():
+                print(f'Generating text for {image}...')
+                open(os.path.join('output', image.split('.')[0] + '.txt'), 'w').write(response['all_text'])
 
 
 if __name__ == '__main__':
